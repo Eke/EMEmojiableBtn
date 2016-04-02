a14926e258f45a29af7656f13b95e30df49475df
Erekle <erekle@itdc.ge>
2016-04-02 21:03:40 +0400

    Release 0.0.1.

---
+++ Example/Pods/Target Support Files/EMEmojiableBtn-iOS7.1/EMEmojiableBtn-iOS7.1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EMEmojiableBtn_iOS7_1 : NSObject
+@end
+@implementation PodsDummy_EMEmojiableBtn_iOS7_1
+@end
---
+++ Example/Pods/Target Support Files/EMEmojiableBtn-iOS8.4/EMEmojiableBtn-iOS8.4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EMEmojiableBtn_iOS8_4 : NSObject
+@end
+@implementation PodsDummy_EMEmojiableBtn_iOS8_4
+@end
