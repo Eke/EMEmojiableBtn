e79e2099634bd4ed686ce8fc94c43fcea40f0d2d
Erekle <erekle@itdc.ge>
2016-04-03 15:46:17 +0400

    config, comment
    Added comments to EMEmojiableBtnConfig.h
    Full ui customization with config

--- Pod/Classes/EMEmojiableBtnConfig.h
+++ Pod/Classes/EMEmojiableBtnConfig.h
@@ -9,9 +9,89 @@
 #import <Foundation/Foundation.h>
 
 @interface EMEmojiableBtnConfig : NSObject
+/**
+ Size of each option
+ */
 @property (assign,nonatomic) CGFloat size;
+
+/**
+ Size of option when it is hilighted
+ */
 @property (assign,nonatomic) CGFloat maxSize;
+
+/**
+ Size of options when one option it is hilighted.
+ When option is hilighted other options are smaller.
+ */
 @property (assign,nonatomic) CGFloat minSize;
+
+/**
+ Spacing between options
+ */
 @property (assign,nonatomic) CGFloat spacing;
+
+/**
+ Bottom space of option selector view to button and information view
+ */
 @property (assign,nonatomic) CGFloat s_options_selector;
+
+/**
+ Background color of screen when options selector is active after UILongPressGestureRecognizer
+ */
+@property (strong,nonatomic) UIColor* backgroundColor;
+
+/**
+ Initial opacity of options listing view
+ */
+@property (assign,nonatomic) CGFloat optionsViewInitialAlpha;
+
+/**
+ Background color of options listing view
+ */
+@property (strong,nonatomic) UIColor* optionsViewBackgroundColor;
+
+/**
+ Shadow color of options listing view
+ */
+@property (strong,nonatomic) UIColor* optionsViewShadowColor;
+
+/**
+ Opacity of options listing shadow
+ */
+@property (assign,nonatomic) CGFloat optionsViewShadowOpacity;
+
+/**
+ Opacity of options listing shadow
+ */
+@property (assign,nonatomic) CGSize optionsViewShadowOffset;
+
+/**
+ Background color of information view
+ */
+@property (strong,nonatomic) UIColor* informationViewBackgroundColor;
+
+/**
+ Dots color of information view
+ */
+@property (strong,nonatomic) UIColor* informationViewDotsColor;
+
+/**
+ Border color of information view
+ */
+@property (strong,nonatomic) UIColor* informationViewBorderColor;
+
+/**
+ Font of information view
+ */
+@property (assign,nonatomic) UIFont* informationViewFont;
+
+/**
+ Text color of information view
+ */
+@property (strong,nonatomic) UIColor* informationViewTextColor;
+
+/**
+ Text for information view. Default : Release to cancel
+ */
+@property (strong,nonatomic) NSString* informationViewText;
 @end
